ff456eb1d20120a65454b33515d76d7925628a15
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-07 23:35:56 +0300

    commit tree 2748a4478e9648227356de67680396c6f5384804

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let greeting;
+  if (distance <= 400) {
+    return `This one is on me!`;
+  }
+  else if (2000 < distance && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return `Ok, sounds good.`;
+  } else {
+    return `No go.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return `Thank you so much.`;
+  case "not as generous":
+    return `Thank you.`;
+    default:
+    return `Bye.`;
+      break;
+  }
 }
